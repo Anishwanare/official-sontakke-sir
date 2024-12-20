b1e4a62cd6c778c122a30fdd5f8900ee28467941
anish <anishwanare9@gmail.com>
2024-12-20 15:57:10 +0530

    backendbugssolved

--- frontend/src/Admin/components/AdminLogin.jsx
+++ frontend/src/Admin/components/AdminLogin.jsx
@@ -78,7 +78,7 @@ const AdminLogin = () => {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
           >
-            {loading ? 'loading....':'Login'}
+            {loading ? 'loading....' : 'Login'}
           </button>
         </form>
       </div>
