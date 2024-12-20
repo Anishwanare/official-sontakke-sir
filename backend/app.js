9557234514c27db617328ae94c3cc0c15cb18f15
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2024-12-20 18:25:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173" || "https://dnyanankur.in"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionSuccessStatus: 200,
