54d290946bc6564d3f4678fd202ea9fafbff41ab
anish <anishwanare9@gmail.com>
2024-12-24 18:42:12 +0530

    bugs

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://dnyanankur.in'], 
+    origin: ['http://dnyanankur.in'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, 
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
