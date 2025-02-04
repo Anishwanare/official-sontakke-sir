7fd64d191adaf309e410ba61006c936c35a3bc3f
anish <anishwanare9@gmail.com>
2025-02-04 16:28:28 +0530

    ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["xlsx"],
+    include: ["xlsx","react-icons"],
   },
   build: {
     commonjsOptions: {
