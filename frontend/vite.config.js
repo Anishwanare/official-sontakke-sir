de114f1eef89b968720360448f16105389ec8d7f
anish <anishwanare9@gmail.com>
2025-02-04 16:30:04 +0530

    ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["xlsx", "react-icons"],
+    include: ["xlsx", "react-icons"],  // Include react-icons in optimized dependencies
   },
   build: {
     commonjsOptions: {
-      include: [/xlsx/, /node_modules/, /react-icons/],  
+      include: [/xlsx/, /node_modules/, /react-icons/],  // Add react-icons to commonjs optimization
+    },
+    rollupOptions: {
+      external: ["react-icons/md"], // Add react-icons/md to external to avoid bundling it
     },
   },
 });
