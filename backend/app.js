26a10aefd8ddadfae3e57ee21f1ef8688a6211e7
anish <anishwanare9@gmail.com>
2025-02-20 02:26:24 +0530

    buggiess

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(fileUpload({
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ["http://localhost:5173" || process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173" ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Add any custom headers
     credentials: true,  // Allow credentials (cookies, etc.)
